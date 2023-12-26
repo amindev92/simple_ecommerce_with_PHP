0b0f6179cb1e94b535125266d504277bf59b34c0
Amin Ataei <amin.ataei92@gmail.com>
2023-12-26 08:22:56 +0330

    connect to database

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+$dbName = "mobocom_store";
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbName);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
